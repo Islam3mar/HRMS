509bb211b51db88ee6bc76af6f401c413eec0cd2
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-09-10 12:42:54 +0300

    OfficialHoliday-Specifications

---
+++ HRMS.Domain/Specifications/OfficialHolidays/OfficialHolidaysOrderedSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HRMS.Domain.Entities;
+
+namespace HRMS.Domain.Specifications.OfficialHolidays
+{
+    public class OfficialHolidaysOrderedSpecification : BaseSpecification<OfficialHoliday>
+    {
+        public OfficialHolidaysOrderedSpecification()
+        {
+            ApplyOrderBy(h => h.Date);
+        }
+    }
+}
